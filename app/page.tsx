89b8c4e560939dd09b47102bb203d66afd3775b2
Kratos1Top <strong.willed343@gmail.com>
2024-04-30 15:59:27 -0300

    added github.api

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+const axios = require('axios').default;
+
 
 async function getData() {
   const res = await fetch('https://api.github.com/users/Artos1tash')
@@ -6,7 +8,7 @@ async function getData() {
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
-  
+  // console.log(res.json())
   return res.json()
 }
 
@@ -15,8 +17,29 @@ export default async function Home() {
   
   return (
     <main>
-      {/* <h1>User data: {data}</h1> */}
-      
+        <h1>
+          {/* Using builtin JSON class to turn data json into a string representation */}
+          User data: {JSON.stringify(data, null, 2)} 
+        </h1>
+        <br></br><br></br>
+        <ul> {/* Second option is here: create a list element, itterate over json and add a list key with the json value*/}
+          {Object.keys(data).map((key, index) => (
+            <li key={index}>{`${key}: ${data[key]}`}</li>
+          ))}
+        </ul>
     </main>
   );
 }
+
+
+// axios.get('https://api.github.com/users/Artos1tash')
+// .then(function (response) {
+//   const login = response.login;
+//   return login;
+// })
+// .catch(function (error) {
+//   return error;
+// })
+// .finally(function () {
+//   return "Done";
+// });
